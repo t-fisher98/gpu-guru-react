b48a26e42fc51b23f62b9dc2f780c672b111a225
t-fisher98 <tyler.fisher98@gmail.com>
2022-02-26 16:33:44 -0700

    Creates Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,9 @@
+function Card(props) {
+  return ( 
+    <header>
+      <h1>Hello</h1>
+    </header>
+  );
+}
+
+export default Card;
---
+++ src/styles/Colors.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Colors = createGlobalStyle`
+  html{
+  --white-200: #f5f5f5;
+  --gray-100: rgb(228, 228, 228);
+  --gray-500: #999;
+  --gray-600: rgb(102, 102, 102);
+  --gray-1000: #252b36;
+  --blue-100: #596782;
+  --text: #333;
+  --border: rgba(0,0,0,.125);
+  }
+`;
+
+export {Colors}
