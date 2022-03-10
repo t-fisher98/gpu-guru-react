797ccdb2fd5d29b0ed1449842697ead8b012d118
Tyler Fisher <tyler.fisher98@gmail.com>
2022-03-10 15:38:19 -0700

    Added components folder and styling to Dashboard.js

---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Box } from '@chakra-ui/react'
+
+function SideBar(props) {
+  return (
+    <Box>
+      <header>
+        <h1>Test</h1>
+      </header>
+    </Box>
+  )
+}
+
+export default SideBar;
--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { 
+  Flex,
+  Box,
+  Button,
+  Link,
+  List,
+  ListItem,
+  ListIcon
+} from '@chakra-ui/react'
+import {
+  BsFillGrid3X3GapFill
+} from 'react-icons/bs'
 
 function DashboardPage() {
+  
+  const [ isOpen, setIsOpen ] = useState(true);
+
+  const toggleSidebar = () => setIsOpen(!isOpen)
+
   return (
-    <header>
-      <h1>Dashboard Page</h1>
-      <a href="/">Login Page</a>
-    </header>
-  )
+    <>
+      <Flex width="100vw" height="100vh" bg="gray.100">
+        <Box
+          width={isOpen ? "15rem" : "2rem"}
+          height="100vh"
+          bg="gray.800"
+        >
+          <List>
+            <ListItem>
+              <Link
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                fontSize="xl"
+                color="white"
+              >
+                <ListIcon as={BsFillGrid3X3GapFill}></ListIcon>
+                Products
+              </Link>
+            </ListItem>
+          </List>
+        </Box>
+        {/* <Button onClick={toggleSidebar}>Slide</Button> */}
+        <Flex>
+
+        </Flex>
+      </Flex>
+    </>
+  );
 }
 
 export default DashboardPage
