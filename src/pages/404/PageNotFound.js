0682614119946edf797ad4273ffed878bd82bc45
Tyler Fisher <tyler.fisher98@gmail.com>
2022-04-04 01:18:13 -0600

    Refactored code & implemented conditional rendering for Back To Safety button on 404 page

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { Center, Heading, Button, Text } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { auth } from "libs/firebase";
 import { BsFillCpuFill } from "react-icons/bs";
-import { Branding } from 'ui/branding'
+import { Center, Heading, Button, Text } from "@chakra-ui/react";
+import { Branding } from "ui/branding";
 
 function PageNotFound() {
   return (
@@ -20,12 +21,14 @@ function PageNotFound() {
       >
         We can't seem to find what you are looking for!
       </Text>
-      <Text color="red.600" fontWeight="bold">Error Code: 404</Text>
-      <Link to="/">
+      <Text color="red.600" fontWeight="bold">
+        Error Code: 404
+      </Text>
+      <Link to="/dashboard">
         <Button colorScheme="blue">Back to Safety</Button>
       </Link>
     </Center>
   );
 }
 
-export default PageNotFound
+export default PageNotFound;
--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -1,48 +1,43 @@
-import React, {useState} from 'react'
-import { useNavigate, Outlet } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
 
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from 'libs/firebase'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "libs/firebase";
 
-import { Flex, Center } from '@chakra-ui/react'
-import { SideBar } from 'components/sidebar'
-import { AppBar } from 'components/appbar'
+import { Flex, Center } from "@chakra-ui/react";
+import { SideBar } from "components/sidebar";
+import { AppBar } from "components/appbar";
 
 function DashboardPage() {
-    // Set up a state variable for conditional rendering based on user authentication
-    const [isUser, setIsUser] = useState(false);
+  // Set up a state variable for conditional rendering based on user authentication
+  const [isUser, setIsUser] = useState(false);
 
-    // Set up a pointer to the useNavigate() function
-    const navigator = useNavigate();
+  // Set up a pointer to the useNavigate() function
+  const navigator = useNavigate();
 
-	onAuthStateChanged(auth, (user) => {
-        // If the user object is null, set the isUser state variable to false and navigate the user to the login page
-        if (!user) {
-            setIsUser(false);
-            navigator("/");
-        }
-
-        // Otherwise, set the isUser state variable to true.
-        setIsUser(true);
-    });
-
-	// If there is no user signed in, do not render any content
-	if (!isUser) {
-        return null;
+  onAuthStateChanged(auth, (user) => {
+    // If the user object is null, set the isUser state variable to false and navigate the user to the login page
+    if (!user) {
+      setIsUser(false);
+      navigator("/");
     }
 
-	// If a user is signed in, render the following content
-    return (
-        <>
-            <Flex width="100vw" height="100vh" bg="gray.200">
-                <SideBar />
-                <AppBar />
-                <Center width="calc(100% - 15.5rem)" flexDirection={"column"}>
-					<Outlet/>
-                </Center>
-            </Flex>
-        </>
-    );
+    // Otherwise, set the isUser state variable to true.
+    setIsUser(true);
+  });
+
+  // Conditionally render the dashboard page based on if a user is logged in or not : {isUser}
+  return ( isUser ? 
+    <>
+      <Flex width="100vw" height="100vh" bg="gray.200">
+        <SideBar />
+        <AppBar />
+        <Center width="calc(100% - 15.5rem)" flexDirection={"column"}>
+          <Outlet />
+        </Center>
+      </Flex>
+    </>
+	: null);
 }
 
-export default DashboardPage
+export default DashboardPage;
